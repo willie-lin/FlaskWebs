834b541972f9b4e0a3f63807091181d27a6886a5
yuan <yyhmmwan@163.com>
2018-10-19 18:47:50 +0800

    add flask-babel

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,7 @@ import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
 
 
 app = Flask(__name__)
@@ -29,6 +30,7 @@ login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 
 if not app.debug:
