0bea3e4ab2eaf09a11e9205c4f3e0f302f7a1dac
yuan <yyhmmwan@163.com>
2018-10-22 21:44:08 +0800

    add translate

--- app/routes.py
+++ app/routes.py
@@ -18,6 +18,7 @@ from app.forms import ResetPasswordRequestForm
 from app.email import send_password_reset_email
 from app.forms import ResetPasswordForm
 from flask_babel import _, get_locale
+from guess_language import guess_language
 
 
 @app.before_request
@@ -34,7 +35,10 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+        post = Post(body=form.post.data, author=current_user, language=language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
